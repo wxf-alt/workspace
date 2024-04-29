3517b9524eb58458bf1ad04a3f837e2baa2ed96c
wxf <wxf18845574579@163.com>
2024-04-29 15:51:42 +0800

    停电影响点数BUG-提交

---
+++ spark-2.4.0/src/main/scala/structured/StructuredDemo.scala
@@ -0,0 +1,42 @@
+package structured
+
+import java.sql.{Connection, DriverManager}
+
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.{DataFrame, Dataset, ForeachWriter, Row, SparkSession}
+
+/**
+ * @Auther: wxf
+ * @Date: 2024/5/15 15:11:38
+ * @Description: StructuredDemo
+ * @Version 1.0.0
+ */
+object StructuredDemo {
+  def main(args: Array[String]): Unit = {
+
+    val spark: SparkSession = SparkSession.builder().master("local[*]").appName("StructuredDemo").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    import spark.implicits._
+
+    val dataDf: DataFrame = spark.readStream
+      .option("host", "localhost")
+      .option("port", 6666L)
+      .format("socket")
+      .load()
+
+    val result = dataDf.as[String].flatMap(_.split(" ")).groupBy("value").count().sort($"count".desc)
+
+    result.writeStream.format("console")
+      .outputMode("complete") //每次将所有的数据写出
+      //      .outputMode("append")
+      //      .outputMode("update")
+      .trigger(Trigger.ProcessingTime(0)) //触发时间间隔,0表示尽可能的快
+      //.option("checkpointLocation","./ckp")//设置checkpoint目录,socket不支持数据恢复,所以第二次启动会报错,需要注掉
+      .start() //开启
+      .awaitTermination() //等待停止
+
+  }
+
+}
---
+++ spark-2.4.0/src/main/scala/structured/StructuredOutMysqlDemo.scala
@@ -0,0 +1,63 @@
+package structured
+
+import java.sql.{Connection, DriverManager}
+
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.{DataFrame, ForeachWriter, SparkSession}
+
+/**
+ * @Auther: wxf
+ * @Date: 2024/5/15 15:11:38
+ * @Description: StructuredDemo
+ * @Version 1.0.0
+ */
+object StructuredOutMysqlDemo {
+  def main(args: Array[String]): Unit = {
+
+    val spark: SparkSession = SparkSession.builder().master("local[*]").appName("StructuredDemo").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    import spark.implicits._
+
+    val dataDf: DataFrame = spark.readStream
+      .option("host", "localhost")
+      .option("port", 6666L)
+      .format("socket")
+      .load()
+
+    val result = dataDf.as[String].flatMap(_.split(" "))
+
+    val mysqlSink = MySqlForeachWrite("jdbc:mysql://localhost:3306/db", "root", "root")
+
+    result.writeStream
+      .foreach(mysqlSink)
+      .outputMode("append")
+      .trigger(Trigger.ProcessingTime(0)) //触发时间间隔,0表示尽可能的快
+      .start() //开启
+      .awaitTermination() //等待停止
+
+
+  }
+
+  case class MySqlForeachWrite(url: String, user: String, pwd: String) extends ForeachWriter[String] {
+
+    var conn: Connection = _
+
+    override def open(partitionId: Long, epochId: Long): Boolean = {
+      Class.forName("com.mysql.jdbc.Driver")
+      conn = DriverManager.getConnection(url, user, pwd)
+      true
+    }
+
+    override def process(value: String): Unit = {
+      val p = conn.prepareStatement("insert into test1(a) values(?)")
+      p.setString(1, value)
+      p.execute()
+    }
+
+    override def close(errorOrNull: Throwable): Unit = {
+      conn.close()
+    }
+  }
+
+}
